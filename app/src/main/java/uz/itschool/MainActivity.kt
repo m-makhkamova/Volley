3522d707d500b87624f96b329797ce558953b4e3
Maftuna Makhkamova <mahkamovamaftuna2007@gmail.com>
2023-10-05 00:40:39 +0500

    Initial commit

---
+++ app/src/main/java/uz/itschool/MainActivity.kt
@@ -0,0 +1,14 @@
+package uz.itschool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val url = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22"
+//        val requestQueue = Voll
+    }
+}
